69881539c270ac99a76605821c1255f893929fd6
Ваше Имя <vlad_moryakov_prog@mail.ru>
2021-04-06 19:56:20 +0300

    all done

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const $ = require('cheerio');
 const cookies = require("./cookies.json");
 const solutions = require("./solutions.json");
+const clipboardy = require('clipboardy');
+
 
 const start = async () => {
     console.log("если что не робит писать мне, не пытайтесь что-то починить сами");
@@ -16,8 +18,9 @@ const start = async () => {
     const page = await browser.newPage();
 
     if (!Object.keys(solutions).length) {
-        console.log("Hello начинаем копирования заданий на ваше устройство");
         if (Object.keys(cookies).length) {
+
+            console.log("Hello начинаем копирования заданий на ваше устройство");
 
             await page.setCookie(...cookies);
 
@@ -25,117 +28,139 @@ const start = async () => {
 
             await copySolutions(page);
 
-
             await start();
 
         } else {
             await login(page, "tanka.bocharova.23@gmail.com", "NoviyOrlean53281TB7");
 
+            await browser.close();
 
             await start();
         }
 
         await logout();
     } else {
-        console.log("запускаем копирования на ваш аккаунт");
+        // console.log("Входим в акк с решениями");
+
+        if (Object.keys(cookies).length) {
+            
+            // console.log("авторизовались в аке с решениями");
+
+            await page.setCookie(...cookies);
+
+            await page.goto("https://lyceum.yandex.ru/", { waitUntil: "networkidle2" });
+
+            await pasteSolutionInAccount(page);
+        } else {
+            // console.log("авторизируемся в аке с решениями");
+            
+            await login(page, "Hronologos227", "cktdfujhs");
+
+            await start();
+        }
+
+
+        await logout();
     }
     await browser.close();
 };
 
-const pasteSolutionInAccount = () => {
+const pasteSolutionInAccount = async (page) => {
 
-    if (Object.keys(cookies).length) {
-
-        await page.setCookie(...cookies);
-
-        await page.goto("https://lyceum.yandex.ru/", { waitUntil: "networkidle2" });
-
-
-        const links = await page.evaluate(() =>
-            Array.from(
-                document.querySelectorAll('a.course-card'), (element) => {
-                    return "https://lyceum.yandex.ru" + element.getAttribute("href");
-                }
-            )
-        )
-
-        await page.goto(links[2], { waitUntil: "networkidle0" });
-
-        const lessonLinks = await page.evaluate(() =>
-            Array.from(
-                document.querySelectorAll('a.link-list__link'), (element) => {
-                    return "https://lyceum.yandex.ru" + element.getAttribute("href");
-                }
-            )
-        )
-
-        for (let k of lessonLinks) {
-            console.log(k);
-            await page.goto(k, { waitUntil: "networkidle0" });
-            const taskLinks = await page.evaluate(() =>
-                Array.from(
-                    document.querySelectorAll('a.student-task-list__task'), (element) => {
-                        return "https://lyceum.yandex.ru" + element.getAttribute("href");
-                    }
-                )
-            )
-
-            for (let i of taskLinks) {
-                await page.goto(i, { waitUntil: "networkidle2" });
-
-                if (!page.url().includes("solutions")) {
-                    console.log("not solutions works");
-
-
-                    const solutionPage = await page.evaluate(() =>
-                        Array.from(
-                            document.querySelectorAll('a.Button2_view_lyceum'), (element) => {
-                                return "https://lyceum.yandex.ru" + element.getAttribute("href");
-                            }
-                        )
-                    )
-
-                    console.log(solutionPage[0]);
-
-                    await page.goto(solutionPage[0], { waitUntil: "networkidle0" });
-
-                    // const taskSolution = await page.evaluate(() => {
-                    //     return document.querySelector('.CodeMirror-line > span')
-                    // })
-
-                    const taskName = await page.evaluate(() => {
-                        return document.querySelector('h1').innerText
-                    })
-
-                    await page.click(".code-editor__control-button");
-
-                    await page.click('.CodeMirror-lines');
-
-                    try {
-                        console.log(taskName, solutions[taskName]);
-                    } catch (e) {
-                        console.log(e);
-                    }
-
-                    // await page.keyboard.type(solutions[taskName]);
-
-                    await page.waitFor(6000);
-
-                } else {
-                    console.log("мы наткнулись на не пусторе решение");
-                }
+    const links = await page.evaluate(() =>
+        Array.from(
+            document.querySelectorAll('a.course-card'), (element) => {
+                return "https://lyceum.yandex.ru" + element.getAttribute("href");
             }
+        )
+    )
 
-            console.log("Все сделано");
+    await page.goto(links[2], { waitUntil: "networkidle0" });
+
+    const lessonLinks = await page.evaluate(() =>
+        Array.from(
+            document.querySelectorAll('a.link-list__link'), (element) => {
+                return "https://lyceum.yandex.ru" + element.getAttribute("href");
+            }
+        )
+    )
+
+    for (let k of lessonLinks) {
+        console.log(k);
+        await page.goto(k, { waitUntil: "networkidle0" });
+        const taskLinks = await page.evaluate(() =>
+            Array.from(
+                document.querySelectorAll('a.student-task-list__task'), (element) => {
+                    return "https://lyceum.yandex.ru" + element.getAttribute("href");
+                }
+            )
+        )
+
+        for (let i of taskLinks) {
+            await page.goto(i, { waitUntil: "networkidle2" });
+
+            if (!page.url().includes("solutions")) {
+                console.log("not solutions works");
+
+
+                const solutionPage = await page.evaluate(() =>
+                    Array.from(
+                        document.querySelectorAll('a.Button2_view_lyceum'), (element) => {
+                            return "https://lyceum.yandex.ru" + element.getAttribute("href");
+                        }
+                    )
+                )
+
+                console.log(solutionPage[0]);
+
+                await page.goto(solutionPage[0], { waitUntil: "networkidle0" });
+
+                // const taskSolution = await page.evaluate(() => {
+                //     return document.querySelector('.CodeMirror-line > span')
+                // })
+
+                const taskName = await page.evaluate(() => {
+                    return document.querySelector('h1').innerText
+                })
+
+                await page.click(".code-editor__control-button");
+
+
+                try {
+                    console.log(taskName, solutions[taskName]);
+                } catch (e) {
+                    console.log(e);
+                }
+
+                await clipboardy.write(solutions[taskName].replace(/[\u200B-\u200D\uFEFF]/g, ''));
+
+                await page.click('.CodeMirror-lines');
+
+                await page.keyboard.down('Control')
+                await page.keyboard.press('V')
+                await page.keyboard.up('Control')
+
+                // for (let t of solutions[taskName].split("\n")) {
+                //     for (let u of t) {
+                //         if (t == " ") {
+                //             await page.keyboard.press('Backspace');
+                //         }
+                //     }
+                //     await page.keyboard.type(t);
+                //     await page.keyboard.press("Enter");
+                //     await page.waitFor(1000);
+                // }
+
+                await page.waitFor(6000);
+
+            } else {
+                console.log("мы наткнулись на не пусторе решение");
+            }
         }
 
-    } else {
-        await login(page, "Hronologos227", "cktdfujhs");
-
-        console.log("Я долбаеб");
-
-        await start();
+        console.log("Все сделано");
     }
+
 }
 
 const login = async (page, login, password) => {
